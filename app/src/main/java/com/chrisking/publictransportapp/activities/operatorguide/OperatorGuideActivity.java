c884680e435101617da1b1accdf1ff9729cf5b06
Chris King <chris@whereismytransport.com>
2018-04-10 16:30:48 +0200

    Fixed portrait mode in some activities, added Flurry analytics to the taxi guide page.

--- app/src/main/java/com/chrisking/publictransportapp/activities/operatorguide/OperatorGuideActivity.java
+++ app/src/main/java/com/chrisking/publictransportapp/activities/operatorguide/OperatorGuideActivity.java
@@ -5,41 +5,33 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.ImageView;
 import android.widget.TextView;
 import com.chrisking.publictransportapp.helpers.Shortcuts;
 
 import com.chrisking.publictransportapp.R;
+import com.flurry.android.FlurryAgent;
 
 public class OperatorGuideActivity extends AppCompatActivity {
-
-    private TextView mNameTextView;
-    private WebView mOperatorGuideWebView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operator_guide);
 
-
-        mOperatorGuideWebView =  (WebView) findViewById(R.id.wv_operator_guide);
-        mNameTextView = (TextView) findViewById(R.id.tv_operator_Name);
-
+        WebView operatorGuideWebView =  (WebView) findViewById(R.id.wv_operator_guide);
+        TextView nameTextView = (TextView) findViewById(R.id.tv_operator_Name);
 
         Intent taxiNameInformationIntent = getIntent();
         if (taxiNameInformationIntent.hasExtra(Intent.EXTRA_TEXT)){
+            FlurryAgent.logEvent("ViewTaxiGuide");
 
-                String taxiName = taxiNameInformationIntent.getStringExtra(Intent.EXTRA_TEXT);
-                mNameTextView.setText(taxiName);
+            String taxiName = taxiNameInformationIntent.getStringExtra(Intent.EXTRA_TEXT);
+            nameTextView.setText(taxiName);
 
-                String resource = Shortcuts.mapOperatorNameToHtmlGuide(taxiName);
-                WebSettings settings = mOperatorGuideWebView.getSettings();
-                settings.setDefaultTextEncodingName("utf-8");
-                mOperatorGuideWebView.loadUrl(resource);
-
-
-
+            String resource = Shortcuts.mapOperatorNameToHtmlGuide(taxiName);
+            WebSettings settings = operatorGuideWebView.getSettings();
+            settings.setDefaultTextEncodingName("utf-8");
+            operatorGuideWebView.loadUrl(resource);
         }
     }
 }
